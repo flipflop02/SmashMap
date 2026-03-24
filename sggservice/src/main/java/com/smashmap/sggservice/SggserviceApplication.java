e4548f698c37d86b99d1e54ae84281cfeca530cb
Florent AUTRET <autret.florent.contact@gmail.com>
2026-03-24 16:58:21 +0100

    Init: premier service spring boot

---
+++ sggservice/src/main/java/com/smashmap/sggservice/SggserviceApplication.java
@@ -0,0 +1,13 @@
+package com.smashmap.sggservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SggserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SggserviceApplication.class, args);
+	}
+
+}
---
+++ sggservice/src/test/java/com/smashmap/sggservice/SggserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smashmap.sggservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SggserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
